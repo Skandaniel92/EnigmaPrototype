a45820ef5af9a36640e717e70d8c38825e165ca3
Skandaniel92 <dani32c4@stud.kea.dk>
2022-02-28 18:16:50 +0100

    Tilføjet metode "bogstavTilTal"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,32 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
+  public static void main(String[] args) {
+
+    Main obj = new Main();
+    int value = obj.bogstavTilTal('G');
+    System.out.println(value);
+
+  }
+
+  public int bogstavTilTal (char bogstav){
+
+    int converter = 64; //Konverterer ASCII-værdi A=1, B=2, osv.
+
+    char ch = bogstav;
+    int ascii = ch - converter;
+
+    return ascii;
+  }
+
+  public char talTilBogstav (int tal) {
+
+
+    return 0;
+  }
+
+
 }
+
+
+
